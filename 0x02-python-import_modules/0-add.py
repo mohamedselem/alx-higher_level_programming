1e39b17d2e6b73514fee36eb7a9012d26dd0e4ef
mohamedselem <the1ne_manga@yahoo.com>
2024-01-03 21:19:17 -0800

    first task

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     """Print the sum of 1 and 2."""
     from add_0 import add
 
-    n = 1
-    c = 2
-    print("{} + {} = {}".format(n, c, add(n, c)))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
