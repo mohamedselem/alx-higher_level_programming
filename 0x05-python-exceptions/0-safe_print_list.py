3709bed4e2dab7f20dbe52bce47a7c31500c4391
mohamedselem <the1ne_manga@yahoo.com>
2024-01-22 10:56:39 -0800

    first task

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+
+    ret = 0
+    for z in range(x):
+        try:
+            print("{}".format(my_list[z]), end="")
+            ret += 1
+        except IndexError:
+            break
+    print("")
+    return (ret)
