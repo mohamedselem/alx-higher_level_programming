ba895e8e7edad1d551fd44fc6c6830dee665da0c
mohamedselem <the1ne_manga@yahoo.com>
2024-01-23 14:38:14 -0800

    all tasks

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""Define the class Square."""
+
+
+class Square:
+    """Represent THE square."""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Define a class Square."""
+
+
+class Square:
+    """define a square."""
+
+    def __init__(self, size):
+        """definition of  a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
